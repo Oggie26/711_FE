436e5682fc6e88754712afa81dd55d0fbd266412
Shin <phanhoangnam17@gmail.com>
2026-05-26 16:14:10 +0700

    confix axios

--- src/service/axiosClient.js
+++ src/service/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://103.75.183.179:8080/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://api.furnimart.click/api',
   headers: {
     'Content-Type': 'application/json',
   },
